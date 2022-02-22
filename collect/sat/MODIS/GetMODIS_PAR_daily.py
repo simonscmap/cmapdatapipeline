50d88565013117f3a8386d9f5614e5d4552f6bd9
dcharing <dharing@uw.edu>
2022-02-22 12:29:27 -0800

    Collect daily PAR data

--- collect/sat/MODIS/GetMODIS_PAR_daily.py
+++ collect/sat/MODIS/GetMODIS_PAR_daily.py
@@ -1,141 +1,43 @@
 import sys
 import os
-import urllib.request
-import requests
+import glob
 
 from cmapingest import vault_structure as vs
-from cmapingest import transfer
+from cmapingest import DB
+from cmapingest import common as cm
 
-vs.makedir(vs.collected_data + "MODIS_PAR_daily_data")
 
+tbl = 'tblModis_PAR'
+
+base_folder = f'{vs.satellite}{tbl}/raw/'
+os.chdir(base_folder)
+max_sql_date = cm.getMax_SQL_date(tbl, 'Rainier')
+last_file_dl = cm.getLast_file_download(tbl, 'satellite')
+
+wget_search = 'wget -q --post-data="sensor=aqua&sdate=2022-01-01&dtype=L3b&addurl=1&results_as_file=0&search=*L3m_DAY_PAR_par_9km*" -O - https://oceandata.sci.gsfc.nasa.gov/api/file_search'
+## https://oceandata.sci.gsfc.nasa.gov/directaccess/MODIS-Aqua/Mapped/Daily/9km/par/
 
 def get_PAR(year, day):
-    start_index = day / 8
-    if day % 8 == 0:
-        start_index = start_index - 1
-    start_index = (start_index * 8) + 1
-
-    end_index = start_index + 7
-    if start_index == 361:
-        end_index = 365
-
-    if (year % 4 == 0) and (start_index == 361):
-        end_index = 366
-    base_folder = vs.collected_data + "/MODIS_PAR_daily_data/"
-    start_index = int(start_index) - 1
-    end_index = int(end_index) - 1
-
     wget_str = (
         "wget --load-cookies ~/.urs_cookies --save-cookies ~/.urs_cookies --auth-no-challenge=on --keep-session-cookies --content-disposition https://oceandata.sci.gsfc.nasa.gov/cgi/getfile/A"
         + str(year)
-        + str(start_index).zfill(3)
+        + str(day).zfill(3)
         + ".L3m_DAY_PAR_par_9km.nc"
     )
-
-    os.chdir(base_folder)
     try:
         os.system(wget_str)
-        os.rename(
-            "A" + str(year) + str(start_index).zfill(3) + ".L3m_DAY_PAR_par_9km.nc.1",
-            "A" + str(year) + str(start_index).zfill(3) + ".L3m_DAY_PAR_par_9km.nc",
-        )
+
     except:
-        print("No file found for date: " + str(year) + str(start_index).zfill(3))
-
-    # url = (
-    #     "https://oceandata.sci.gsfc.nasa.gov/cgi/getfile/A"
-    #     + str(year)
-    #     + str(start_index).zfill(3)
-    #     + ".L3m_DAY_PAR_par_9km.nc"
-    # )
-    # path = base_folder + "MODIS_PAR_daily_" + str(year) + str(day).zfill(3) + ".nc"
-    # print("Downloading: " + url)
-    # print(path)
-    # try:
-    #     urllib.request.urlretrieve(url, path)
-    # except:
-    #     print(
-    #         "No file found for date: "
-    #         + str(year)
-    #         + str(start_index).zfill(3)
-    #         + str(year)
-    #         + str(end_index)
-    #     )
+        print("No file found for date: " + str(year) + str(day).zfill(3))
 
 
-# year_list = range(2010, 2014, 1)
-year_list = range(2014, 2015, 1)
+year_list = range(2020, 2021, 1)
 
-startDay = int(1)
+startDay = int(28)
 endDay = int(365)
-
+year = 2020
 
 for year in year_list:
     for day in range(startDay, endDay + 1):
         get_PAR(year, day)
 
-
-# import sys
-# import os
-# import urllib.request
-# import requests
-
-# sys.path.append("../../config")
-# import config_vault as cfgv
-
-# sys.path.append("../../DBIngest/login")
-# import credentials as cr
-
-# """ 9km spatial resolution """
-
-# def get_PAR(year, day):
-#     start_index = day / 8
-#     if day % 8 == 0:
-#         start_index = start_index - 1
-#     start_index = (start_index * 8) + 1
-
-#     end_index = start_index + 7
-#     if start_index == 361:
-#         end_index = 365
-
-#     if (year % 4 == 0) and (start_index == 361):
-#         end_index = 366
-#     base_folder = cfgv.rep_MODIS_PAR_daily_raw
-#     start_index = int(start_index) - 1
-#     end_index = int(end_index) - 1
-
-#     wget_str = (
-#         "wget --load-cookies ~/.urs_cookies --save-cookies ~/.urs_cookies --auth-no-challenge=on --keep-session-cookies --content-disposition https://oceandata.sci.gsfc.nasa.gov/cgi/getfile/A"
-#         + str(year)
-#         + str(start_index).zfill(3)
-#         + ".L3m_DAY_PAR_par_9km.nc"
-#     )
-
-#     os.chdir(base_folder)
-#     print("A" + str(year) + str(start_index).zfill(3) + ".L3m_DAY_PAR_par_9km.nc.1")
-#     try:
-#         os.system(wget_str)
-#         os.rename(
-#             "A" + str(year) + str(start_index).zfill(3) + ".L3m_DAY_PAR_par_9km.nc.1",
-#             "A" + str(year) + str(start_index).zfill(3) + ".L3m_DAY_PAR_par_9km.nc",
-#         )
-#     except:
-#         print(
-#             "No file found for date: "
-#             + str(year)
-#             + str(start_index).zfill(3)
-#             + str(year)
-#             + str(end_index)
-#         )
-
-
-# # year_list = range(2015,2020, 1)
-# year_list = range(2020, 2021, 1)
-
-# startDay = int(1)
-# endDay = int(365)
-
-
-# for year in year_list:
-#     for day in range(startDay, endDay + 1):
-#         get_PAR(year, day)
